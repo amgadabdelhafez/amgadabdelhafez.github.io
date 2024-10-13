192a6366bd498c98a6c2c8e760c2698f5d3b59e3
amgadabdelhafez <amgad@yahoo.com>
2024-10-13 02:05:02 -0700

    fixing getChapterNamee

--- repo/repository.skipintro/default.py
+++ repo/repository.skipintro/default.py
@@ -84,17 +84,103 @@ class SkipIntroPlayer(xbmc.Player):
             xbmc.log(f'skipintro: Error in testRpc: {str(e)}', xbmc.LOGERROR)
             
     def onAVStarted(self):
-        xbmc.log('skipintro: AV started', xbmc.LOGDEBUG)
+        xbmc.log('skipintro: onAVStarted method called', xbmc.LOGINFO)
         if not self.bookmarks_checked:
             self.check_for_intro_chapter()
         
         xbmc.log('skipintro: testing rpc', xbmc.LOGDEBUG)
         self.testRpc()
 
-    # ... [rest of the class remains unchanged]
+    def check_for_intro_chapter(self):
+        xbmc.log('skipintro: Checking for intro chapter', xbmc.LOGDEBUG)
+        playing_file = self.getPlayingFile()
+        if not playing_file:
+            xbmc.log('skipintro: No playing file detected', xbmc.LOGERROR)
+            return
+
+        # Retrieve chapters
+        chapters = self.getChapters()
+        if chapters:
+            xbmc.log('skipintro: Found {} chapters'.format(len(chapters)), xbmc.LOGDEBUG)
+            self.intro_bookmark = self.find_intro_chapter(chapters)
+            if self.intro_bookmark:
+                self.prompt_skip_intro()
+            else:
+                self.bookmarks_checked = True
+        else:
+            self.check_for_default_skip()
+
+    def getChapters(self):
+        xbmc.log('skipintro: Getting chapters', xbmc.LOGDEBUG)
+        try:
+            jsonrpc = xbmc.executeJSONRPC
+            result = jsonrpc('{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}')
+            players = json.loads(result)['result']
+            if not players:
+                xbmc.log('skipintro: No active players found', xbmc.LOGDEBUG)
+                return []
+
+            player_id = players[0]['playerid']
+            chapter_info = json.loads(jsonrpc(json.dumps({
+                "jsonrpc": "2.0",
+                "method": "Player.GetChapters",
+                "params": {"playerid": player_id, "properties": ["name", "title", "time"]},
+                "id": 1
+            })))
+            
+            chapters = []
+            if 'result' in chapter_info and 'chapters' in chapter_info['result']:
+                for chapter in chapter_info['result']['chapters']:
+                    name = chapter.get('name') or chapter.get('title') or f"Chapter {len(chapters) + 1}"
+                    time = chapter['time'] / 1000  # Convert milliseconds to seconds
+                    xbmc.log(f'skipintro: Chapter: Name: {name}, Time: {time}', xbmc.LOGDEBUG)
+                    chapters.append({'name': name, 'time': time})
+            else:
+                xbmc.log("skipintro: No chapters found for this video.", xbmc.LOGDEBUG)
+            
+            return chapters
+        except Exception as e:
+            xbmc.log(f'skipintro: Error in getChapters: {str(e)}', xbmc.LOGERROR)
+            return []
+
+    def find_intro_chapter(self, chapters):
+        xbmc.log('skipintro: Searching for intro chapter', xbmc.LOGDEBUG)
+        for chapter in chapters:
+            xbmc.log('skipintro: Checking chapter: {} at {} seconds'.format(chapter['name'], chapter['time']), xbmc.LOGDEBUG)
+            if 'intro end' in chapter['name'].lower():
+                xbmc.log('skipintro: Intro end chapter found at {} seconds'.format(chapter['time']), xbmc.LOGINFO)
+                return chapter['time']
+        xbmc.log('skipintro: No intro end chapter found', xbmc.LOGINFO)
+        return None
+
+    def check_for_default_skip(self):
+        xbmc.log('skipintro: Checking for default skip', xbmc.LOGDEBUG)
+        if self.default_skip_checked:
+            return
+
+        current_time = self.getTime()
+        xbmc.log('skipintro: Current time: {}'.format(current_time), xbmc.LOGDEBUG)
+        if current_time >= self.default_delay:
+            self.intro_bookmark = current_time + self.skip_duration
+            self.prompt_skip_intro()
+
+        self.default_skip_checked = True
+
+    def prompt_skip_intro(self):
+        xbmc.log('skipintro: Prompting user to skip intro', xbmc.LOGDEBUG)
+        skip_intro = xbmcgui.Dialog().yesno('Skip Intro?', 'Do you want to skip the intro?')
+        if skip_intro:
+            self.skip_to_intro_end()
+
+    def skip_to_intro_end(self):
+        if self.intro_bookmark:
+            xbmc.log('skipintro: Skipping intro to {} seconds'.format(self.intro_bookmark), xbmc.LOGINFO)
+            self.seekTime(self.intro_bookmark)
+        else:
+            xbmc.log('skipintro: No intro bookmark set to skip', xbmc.LOGERROR)
 
 if __name__ == '__main__':
-    xbmc.log('skipintro: Starting SkipIntroPlayer', xbmc.LOGDEBUG)
+    xbmc.log('skipintro: Script execution started', xbmc.LOGINFO)
     player = SkipIntroPlayer()
     monitor = xbmc.Monitor()
 
